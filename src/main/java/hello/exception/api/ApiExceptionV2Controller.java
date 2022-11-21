439fd1a38a3b3874f3f11f2a158d995b44960d4d
kk210001 <91827665+kk210001@users.noreply.github.com>
2022-11-21 20:49:23 +0900

    @ExceptionHandler 예외 발생시 ExceptionResolver 가서 ExceptionHandlerExceptionHandler 실행 됨 @ExceptionHandler 존재 시 이것이 우선으로 호출
    정상적인 흐름으로 바뀌어 status 200으로 변환 됨
    예외 상태 코드도 바꿀려면?

---
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -0,0 +1,62 @@
+package hello.exception.api;
+
+
+import hello.exception.exception.BadRequestException;
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@RestController
+public class ApiExceptionV2Controller {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+    @GetMapping("/api2/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping(value = "/api2/response-status-ex1",produces = "application/json;charset=utf8")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping(value = "/api2/response-status-ex2",produces = "application/json;charset=utf8")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/api2/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+
+    }
+}
